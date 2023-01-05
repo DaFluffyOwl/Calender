a056122386672e94b4c1a2b2388ab960e22eafbc
DaFluffyOwl <creeper47@outlook.com>
2023-01-05 13:56:18 -0500

    Added some comments

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ Sun    Mon    Tue     Wed    Thu     Fri    Sat
 char *days[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
 char *currMonth[1] = {"January"};
 int year = 2023;
-int start_day = 3;
+int start_day = 1;
 
 void print_days();
 void print_date();
@@ -26,6 +26,7 @@ int main(){
 }
 
 void print_days(){
+    //Prints day names
     for(int i = 0; i < 7; i++){
         printf("%s\t", days[i]);
     }
@@ -33,10 +34,11 @@ void print_days(){
 }
 
 void print_date(){
-    int less_space = 0;
+    //Allows for any start date
     for(int d = 0; d < (start_day-1); d++){
         printf("\t");
     }
+    //Prints each day number
     for(int x = 1; x <= 31; x++){
         printf("%d\t", x);
         if((x+start_day-1) % 7 == 0){
